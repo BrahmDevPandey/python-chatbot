faea474dc6c29d971d3a3211953141fffa12a67b
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-01-16 23:35:28 +0530

    updated /bot route

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello there! Send requests to /bot to get responses'
+    return 'Send requests to /bot to get responses'
 
 @app.route("/bot", methods=["POST", "GET"])
 def response():
-    query = "" + dict(request.form)['query']
+    query = dict(request.form)['query']
     result = query + " " + time.ctime()
     return jsonify({"response" : result})
 
