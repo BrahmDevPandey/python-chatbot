ccc287d174f2120dc50b9cfdb8f0b62d360cf865
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-01-16 21:30:04 +0530

    added vercel config file

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, jsonify, request
 import time
 
 app = Flask(__name__)
-@app.route("/bot", method=["POST"])
 
+@app.route('/')
+def hello_world():
+    return 'Hello there! Send requests to /bot to get responses'
+
+@app.route("/bot", methods=["POST", "GET"])
 def response():
     query = dict(request.form)['query']
     result = query + " " + time.ctime()
     return jsonify({"response" : result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.debug = True
+    app.run(host="0.0.0.0", port="5500")
