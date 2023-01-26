b0f405f355f7bf348ff66f8dbaca33d9a84d38af
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-01-27 04:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import time
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def hello_world():
     return 'Send requests to /bot to get responses'
 
